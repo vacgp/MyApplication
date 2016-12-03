37c101e609ceefb9595005c99d1037e86b49399b
vacgp <vac.gp2@gmail.com>
2016-12-03 16:37:28 +0300

    xml changes

--- app/src/main/java/com/example/mayoo/myapplication/Home.java
+++ app/src/main/java/com/example/mayoo/myapplication/Home.java
@@ -1,6 +1,7 @@
 package com.example.mayoo.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -26,6 +27,10 @@ public class Home  extends AppCompatActivity {
         to_intent.putExtra("username", username); // (Key, Value)
         startActivity(to_intent);
 
+
+    }
+
+    public void ask_a_doctor(View view) {
 
     }
 }
